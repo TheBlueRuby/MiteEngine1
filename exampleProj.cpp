844277735834f38f27f9c00fa20c4c9745f9ae35
TheBlueRuby <maxnyeholt@gmail.com>
2022-07-15 18:41:08 +1000

    Fixed windowY define

---
+++ exampleProj.cpp
@@ -0,0 +1,10 @@
+#include "miteengine1.hpp"
+
+int main(int argc, char* argv[])
+{
+    windowX = 960;
+    windowY = 640;
+    //Call the function in the header file to start. 
+    //Input args are argc, argv and a string for the window title
+    miteEngine1Start(argc, argv, "Example Project using Mite Engine 1");
+}
--- miteengine1.hpp
+++ miteengine1.hpp
@@ -21,7 +21,7 @@
     #define M_PI 3.14159265358979325406
 #endif
 
-int windowX, int windowY;
+int windowX, windowY;
 
 float frame1, frame2, fps;
 int gameState=0, timer=0;
